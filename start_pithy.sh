c4c7ad631407138ad7a0148e3807729be94213bc
Barry Van Tassell <bajavata@gmail.com>
2016-06-07 15:48:12 -0700

    fixed start_pithy.sh

--- start_pithy.sh
+++ start_pithy.sh
@@ -2,5 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-forever start $DIR/pithy/index.js 8001 --codebase=$DIR/code --histbase=$DIR/code_stamped > $DIR/pithy.log
+cd $DIR/pithy
+
+forever start index.js 8001 --codebase=../code --histbase=../code_stamped > ../logs/pithy.log
 
