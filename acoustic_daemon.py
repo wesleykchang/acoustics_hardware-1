fef398b35a041b935d19ab75f978821edb0abbdc
awilkinson88 <kasja525@gmail.com>
2016-10-27 10:40:41 -0700

    quasi-security added for UI daemon

--- acoustic_daemon.py
+++ acoustic_daemon.py
@@ -13,9 +13,13 @@ from flask_socketio import SocketIO, send, emit
 import matplotlib
 from matplotlib import pyplot as plt
 import mpld3
+from datetime import timedelta
 # import plotter #anne's plotting library
 
-from flask import Flask, send_from_directory, request
+from flask_wtf import Form, validators
+from wtforms import StringField, PasswordField
+from flask import Flask, send_from_directory, request, Response, render_template, redirect, url_for
+from flask_login import LoginManager, UserMixin, login_required, login_user
 
 import eventlet
 
@@ -24,8 +28,48 @@ eventlet.monkey_patch() #fuuuuuck
 __all__ = ["AcousticDaemon"]
 app = Flask(__name__)
 app.config['DEBUG'] = False
+app.config["SECRET_KEY"] = '\x94\xec:\xaf\xeb\xc7.\xd5\xe8J1\xbbX\xac\xa5\x92\xd3\xb0k9\x14\xc6\xe7c'
 #app.config['HOST'] = "0.0.0.0"  #can change this to whatever
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "/login"
+login_manager.session_protection = "strong"
+
 socketio = SocketIO(app, binary=True)
+
+class User(UserMixin):
+    def __init__(self, username, password):
+        self.id = username
+        self.password = password
+
+    def check_password(self,field_pword):
+        return field_pword == self.password
+
+    @classmethod
+    def get(cls,id):
+        user_database = {
+                "Lab": User("Lab", "batteriesarelikerocks")}
+        return user_database.get(id)
+
+class LoginForm(Form):
+    username = StringField('Username')
+    password = PasswordField('Password')
+
+    def __init__(self, *args, **kwargs):
+        Form.__init__(self, *args, **kwargs)
+        self.user = None
+
+    def validate(self):
+        user = User.get(self.username.data)
+        if user is None:
+            return False
+
+        if not user.check_password(self.password.data):
+            return False
+
+        self.user = user
+        return True
 
 
 class AcousticDaemon(Daemon):
@@ -69,28 +113,48 @@ class UIDaemon(Daemon):
             info['data'].append(row) #append to the list of dicts
             json.dump(info, open(logname, 'w'))
 
+
+        @login_manager.user_loader
+        def load_user(user_id):
+            return User.get(user_id)
+
         @app.route('/')
+        @login_required
         def root():
             return send_from_directory('static','index.html')
 
+        @app.route('/login', methods=['GET', 'POST'])
+        def login():
+            form = LoginForm()
+            if form.validate_on_submit():
+                login_user(form.user, remember=False)
+                return redirect('/')
+            return render_template('login.html', form=form)
+
+
         @app.route('/table_load')
+        @login_required
         def table_load():
             return open("table_state.json").read()
 
         @app.route('/fonts/<path:filename>') #important for being able to load font files
+        @login_required
         def custom_static(filename):
             return send_from_directory("fonts", filename)
 
         @app.route('/<month>/<day>/<year>/view')
+        @login_required
         def view_table(month,day,year):
             return send_from_directory('static/tableviewer','index.html') #show logfile
 
         @app.route('/<month>/<day>/<year>/table_load')
+        @login_required
         def log_load(month,day,year):
             startdate = year + '_' + month + '_' + day
             return open(os.path.join("../Data",startdate,"logfile.json")).read() #get data for a given log
 
         @app.route('/table_save', methods=['GET', 'POST'])
+        @login_required
         def table_save():
             # if request.method == 'POST':
                 out = {}
@@ -105,6 +169,7 @@ class UIDaemon(Daemon):
                 return json.dumps(out)
 
         @app.route('/<month>/<day>/<year>/del_test', methods=['GET', 'POST'])
+        @login_required
         def del_test(month,day,year):
             if request.method == 'POST':
                 postdata = json.loads(request.get_data().decode('utf-8'))
@@ -122,10 +187,12 @@ class UIDaemon(Daemon):
                 return "ok"
 
         @app.route('/<month>/<day>/<year>/viewfigs')
+        @login_required
         def viewfigs(month,day,year):
             return send_from_directory('static/figviewer','index.html')
 
         @app.route('/<month>/<day>/<year>/<testid>/makefigs')
+        @login_required
         def makefig(month,day,year,testid):
             start_date = year + '_' + month + '_' + day
             files = os.listdir(os.path.join('../Data',start_date,testid))
@@ -147,10 +214,6 @@ class UIDaemon(Daemon):
             plt.close(fig)
             return json.dumps(out)
 
-        @app.route('/fsweep')
-        def sweep_fs():
-            return send_from_directory('static','sweepdex.html')
-
         @socketio.on('test')
         def handle_test(data):
             socketio.emit('update', data) #tell the JS to update.
@@ -161,7 +224,7 @@ class UIDaemon(Daemon):
                         
         while True:
             # socketio.run(app,host="0.0.0.0",port=5000)  
-            socketio.run(app,host="0.0.0.0",port=5000)  
+            socketio.run(app,port=self.port)  
 
     def loadTools(self):
         pass
@@ -171,7 +234,7 @@ if __name__=="__main__":
     pulserurl = 9003
     muxurl = 9002
     host = "0.0.0.0"
-    port = 5000
+    port = 6054
     for i in sys.argv:
         if i.find("=") > 0: 
             print(i)
@@ -180,5 +243,5 @@ if __name__=="__main__":
     d = UIDaemon(port,host)
     d.start()
     time.sleep(1)
-    ad = AcousticDaemon(uiurl=port,muxurl=None,muxtype=None,pulserurl=pulserurl)
-    ad.start()
+    # ad = AcousticDaemon(uiurl=port,muxurl=None,muxtype=None,pulserurl=pulserurl)
+    # ad.start()
