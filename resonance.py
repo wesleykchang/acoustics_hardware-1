59e88a99ef2d8253bf319a867d2318ec74d896ab
lab <lab@canismajor.lbl.gov>
2017-05-15 09:57:47 -0700

    pushing changes to sweep function

--- resonance.py
+++ resonance.py
@@ -21,7 +21,7 @@ import utils
 sys.path.append('../EASI-analysis/analysis')
 from filesystem import Saver
 import data
-# from libPicoscope import Picoscope
+from libPicoscope import Picoscope
 import numpy as np
 import time
 from scipy.optimize import fsolve
@@ -63,7 +63,7 @@ def segments(start_f,stop_f,sweep_t,sample_factor=5):
     k = (stop_f-start_f)/sweep_t #how quickly the chirp increases
     t_total = 0
     chirp_list = []
-    t_list = np.array([])
+    t_list = []
     # t_arrays = []
 
     i = 0 #index for how many loops. used to increase samples
@@ -72,7 +72,10 @@ def segments(start_f,stop_f,sweep_t,sample_factor=5):
         i += 1
         func = lambda t : sample_factor*k*t**2 + sample_factor*start_f*t - max_samples*i
         t_i = fsolve(func,1)[0] - t_total
-        t_array = np.linspace(t_total,t_total+t_i, max_samples)
+        if (t_total+t_i) > sweep_t:
+            t_array = np.linspace(t_i,sweep_t,max_samples)
+        else:
+            t_array = np.linspace(t_total,t_total+t_i, max_samples)
         # t_arrays.append(t_array)
         chirp_i = np.sin(2*np.pi*(start_f*t_array + (k/2)*t_array**2))
         # plt.plot(t_array,chirp_i,'-')
@@ -167,21 +170,40 @@ if __name__ == '__main__':
         "num_freqs" : num_freqs
     }
 
-    ps = Picoscope(avg_num=0, resonance=True, duration = sweep_t,maxV=.1,sample_rate=sample_rate)
+    ps = Picoscope(avg_num=0, resonance=True, duration = sweep_t,maxV=2,sample_rate=sample_rate) ###Change this to be dynamic!!
     s = Saver()
     try:
         for i in range(num_sweeps):
+            print(len(sys.argv),sys.argv[4])
             if len(sys.argv) == 6 and sys.argv[5]== 'arb':
                 t_list, chirp_list = segments(start_f,stop_f,sweep_t)
                 i = 0
                 total_data = np.array([])
                 total_ts = np.array([])
                 for time in t_list:
-                    ps.sample_rate = (32000/time[2])
+                    print(time)
+                    # ps.sample_rate = (32000/time[2])
                     w_data = ps.generate_waveform(chirp_list[i], time[2])
-                    total_data.append(w_data)
-                    sample_ts = np.linspace(time[0],time[1],len(w_data))
+                    # ps.signal_generator(frequency=1e6, shots=1) #necessary for returning the picoscope to 0
+                    # ps.signal_generator(frequency=1e6, shots=1) #necessary for returning the picoscope to 0
+                    sample_ts = np.linspace(time[0],time[1],len(w_data[1]))
+                    # plt.plot(sample_ts, w_data[1])
+                    # plt.show()
+                    total_data = np.append(total_data,w_data[1])
+                    total_ts = np.append(total_ts, sample_ts)
                     i += 1
+
+                    seg_wave = data.Wave(amps=w_data[1],framerate=sample_rate,delay=time[0])
+                    seg_wave.plot(scale_x=False)
+                    plt.show()
+
+                    seg_spec = seg_wave.to_spectrum()
+                    seg_spec.plot()
+                    plt.show()
+                print(total_ts)
+                print(total_data)
+                plt.plot(total_ts,total_data,'b-')
+                plt.show()
 
             else:
                 w_data = ps.signal_generator(frequency=start_f, stopFreq=stop_f, shots=0, numSweeps=1, increment=inc, dwellTime=dwelltime)
@@ -192,11 +214,11 @@ if __name__ == '__main__':
             # w_s = w.to_spectrum()
             # w_s.plot()
             # plt.show()
-            if i == 0:
-                specsum = w_s
-            else:
-                # wavesum = wavesum + w_data[1]
-                specsum += w_s
+            # if i == 0:
+            #     specsum = w_s
+            # else:
+            #     # wavesum = wavesum + w_data[1]
+            #     specsum += w_s
             # time.sleep(2.5) #give the power amp a chance to stabilize between tests
 
         # specsum.hs = specsum.hs/num_sweeps
@@ -213,22 +235,22 @@ if __name__ == '__main__':
         import traceback
         print(traceback.format_exc())
 
-    avg_data = np.mean(mean_array,axis=0)
-    wav = data.wave(amps=avg_data,framerate=sample_rate)
-    wav.plot(scale_x=False)
-    plt.show()
+    # avg_data = np.mean(mean_array,axis=0)
+    # wav = data.wave(amps=avg_data,framerate=sample_rate)
+    # wav.plot(scale_x=False)
+    # plt.show()
 
-    spec = wav.to_spectrum()
-    spec.plot()
-    plt.show()
+    # spec = wav.to_spectrum()
+    # spec.plot()
+    # plt.show()
 
-    spec_total = spec_list[0]
-    for spectrum in spec_list[1:]:
-        spec_total += spectrum
-    spec_total.hs  = spec_total.hs/(len(spec_list))
-    spec_total.plot()
-    plt.show()
+    # spec_total = spec_list[0]
+    # for spectrum in spec_list[1:]:
+    #     spec_total += spectrum
+    # spec_total.hs  = spec_total.hs/(len(spec_list))
+    # spec_total.plot()
+    # plt.show()
 
-    incTable(filename)
-    s.saveData(data,row,None)
+    # incTable(filename)
+    # s.saveData(data,row,None)
 
