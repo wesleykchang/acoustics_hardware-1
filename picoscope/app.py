0a20d0ab313193f9b900cf3342b8c5a741ea7e5d
gunnarthorsteins <gunnarthorsteins@outlook.com>
2022-07-11 16:27:50 -0400

    bug: going to get this damn flask server working

--- picoscope/app.py
+++ picoscope/app.py
@@ -5,30 +5,34 @@ import picoscope
 
 PORT = '5001'
 
-if __name__ == "__main__":
-    #start up the picoscope connection
-    app = flask.Flask("pico")
-    ps = picoscope.Picoscope()
+app = flask.Flask("pico")
+
+#start up the picoscope connection
+# Initializing class without connecting to picoscope
+ps = picoscope.Picoscope()
+
+#test to see that server is alive
+@app.route('/')
+def hello_world():
+    return "Flask Picoscope server: running"
+
+# Connect to picoscope
+# Keeping it here because the wrapper connection
+# tends to fail a couple of times
+@app.route('/connect')
+def connect():
+    ps.connect()
+
+    return "Picoscope connected"
+
+# Runs resonance
+@app.route('/get_resonance', methods=['POST'])
+def get_resonance():
+    params = flask.request.values    
+    data = ps.sweep(params=params)
+
+    return json.dumps(data)
     
-    #test to see that server is alive
-    @app.route('/')
-    def hello_world():
-        return "Flask Picoscope server: running"
 
-    @app.route('/connect')
-    def connect():
-        ps.connect()
-
-        return "Picoscope connected"
-
-    # Runs resonance
-    @app.route('/get_resonance', methods=['POST'])
-    def get_resonance():
-        params = flask.request.values    
-        data = ps.sweep(params=params)
-
-        return json.dumps(data)
-        
-
-    app.run(port=PORT,host="0.0.0.0")
+app.run(port=PORT,host="0.0.0.0")
 
