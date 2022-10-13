1de5ad5afc10a34f28ae12c01184363ee9d7aa1d
steingartlab <steingartlab@gmail.com>
2022-10-12 20:48:45 -0400

    bug: type hints ffs

--- picoscope/sweep.py
+++ picoscope/sweep.py
@@ -3,16 +3,12 @@ in appropriate sequence.
 """
 
 from picoscope.picoscope import Picoscope4000
-from picoscope.utils import (
-    get_no_frequencies,
-    parse_voltage_range,
-    set_input_channel,
-    set_sampling_params,
-    ParsedParams
-)
+from picoscope.utils import (get_no_frequencies, parse_voltage_range,
+                             set_input_channel, set_sampling_params,
+                             ParsedParams)
 
 
-def sweep(picoscope_: Picoscope4000, params: ParsedParams) -> list(float):
+def sweep(picoscope_: Picoscope4000, params: ParsedParams) -> list:
     '''Wrapper for frequency sweep.
 
     Follows recommended block mode procedure as
@@ -32,18 +28,14 @@ def sweep(picoscope_: Picoscope4000, params: ParsedParams) -> list(float):
 
     channel = set_input_channel(params=params)
     enum_voltage_range = parse_voltage_range(
-        numerical_voltage_range=params['voltage_range']
-    )
-    no_frequencies = get_no_frequencies(
-        start_freq=params['start_freq'],
-        end_freq=params['end_freq'],
-        increment=params['increment']
-    )
+        numerical_voltage_range=params['voltage_range'])
+    no_frequencies = get_no_frequencies(start_freq=params['start_freq'],
+                                        end_freq=params['end_freq'],
+                                        increment=params['increment'])
     enum_sampling_interval, no_samples = set_sampling_params(
         no_samples=params['max_samples'],
         dwell=params['dwell'],
-        no_frequencies=no_frequencies
-    )
+        no_frequencies=no_frequencies)
 
     c_buffer = picoscope_.make_buffer(no_samples_=no_samples)
 
@@ -55,32 +47,24 @@ def sweep(picoscope_: Picoscope4000, params: ParsedParams) -> list(float):
     picoscope_.define_procedure(**params)
 
     # 2. Select channel ranges and AC/DC coupling
-    picoscope_.set_channel_params(
-        enum_voltage_range=enum_voltage_range,
-        channel=channel
-    )
+    picoscope_.set_channel_params(enum_voltage_range=enum_voltage_range,
+                                  channel=channel)
 
     # 3. Select timebases
-    picoscope_.get_timebase(
-        enum_sampling_interval=enum_sampling_interval,
-        no_samples=no_samples
-    )
+    picoscope_.get_timebase(enum_sampling_interval=enum_sampling_interval,
+                            no_samples=no_samples)
 
     # 4. Trigger setup
     picoscope_.set_simple_trigger(channel=channel)
 
     # 7. Tell the driver where the memory buffer is
-    picoscope_.set_data_buffer(
-        channel=channel,
-        no_samples=no_samples,
-        c_buffer=c_buffer
-    )
+    picoscope_.set_data_buffer(channel=channel,
+                               no_samples=no_samples,
+                               c_buffer=c_buffer)
 
     # 5. Start collecting data
-    picoscope_.run_block(
-        enum_sampling_interval=enum_sampling_interval,
-        no_samples=no_samples
-    )
+    picoscope_.run_block(enum_sampling_interval=enum_sampling_interval,
+                         no_samples=no_samples)
 
     # 5.5 Start arbitrary wave generator
     picoscope_.pull_trigger()
@@ -94,10 +78,8 @@ def sweep(picoscope_: Picoscope4000, params: ParsedParams) -> list(float):
     # 9. Teardown
     picoscope_.stop()
 
-    data_mV = picoscope_.to_mV(
-        enum_voltage_range=enum_voltage_range,
-        no_samples=no_samples,
-        c_buffer=c_buffer
-    )
+    data_mV = picoscope_.to_mV(enum_voltage_range=enum_voltage_range,
+                               no_samples=no_samples,
+                               c_buffer=c_buffer)
 
     return data_mV
